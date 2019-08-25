013787128e5508cc50b186985c62d35c91cb761c
John <john@cupist.com>
2019-08-25 16:18:19 +0900

    Modify Users model
    1. Install sqlalchemy-utils for ChoiceType field
    2. Add follow model

---
+++ apps/users/models/user_model.py
@@ -0,0 +1,38 @@
+from sqlalchemy import (Column, BigInteger, Unicode, Table, ForeignKey,
+                        UniqueConstraint)
+from sqlalchemy.orm import relationship
+from sqlalchemy_utils import ChoiceType
+
+from core.databases import Base
+from core.databases.mixin import TimestampMixin
+
+follows = Table(
+    'follows',
+    Base.metadata,
+    Column('follower_id', BigInteger, ForeignKey('users.id')),
+    Column('following_id', BigInteger, ForeignKey('users.id')),
+    UniqueConstraint('follower_id', 'following_id', name='unique_follows')
+)
+
+
+class User(Base, TimestampMixin):
+    __tablename__ = 'users'
+
+    GENDER_TYPES = [
+        (u'm', u'M'),
+        (u'f', u'F'),
+        (u'u', u'Unknown'),
+    ]
+
+    id = Column(BigInteger, autoincrement=True, primary_key=True)
+    name = Column(Unicode(length=255), nullable=True)
+    website = Column(Unicode(length=50), nullable=True)
+    bio = Column(Unicode(length=50), nullable=True)
+    phone = Column(Unicode(length=20), nullable=True)
+    gender = Column(ChoiceType(GENDER_TYPES))
+    follower = relationship(
+        'User',
+        secondary=follows,
+        primaryjoin=id==follows.c.follower_id,
+        secondaryjoin=id==follows.c.following_id,
+    )
