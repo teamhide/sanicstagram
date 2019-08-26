d03c9f8a2bfd0f9bffedcf41e303085070aee580
john <john@cupist.com>
2019-08-26 11:34:18 +0900

    Add UnFollowUser frame to views

--- apps/users/views/v1/user_view.py
+++ apps/users/views/v1/user_view.py
@@ -3,8 +3,10 @@ from typing import Union, NoReturn
 from sanic.request import Request
 from sanic.response import json
 from sanic.views import HTTPMethodView
-from apps.users.dtos import FollowUserDto
-from apps.users.schemas import FollowUserRequestSchema
+
+# from apps.users.dtos import FollowUserDto
+from apps.users.schemas import (FollowUserRequestSchema,
+                                UnFollowUserRequestSchema)
 from core.exceptions import ValidationErrorException
 from core.utils import TokenHelper
 
@@ -44,4 +46,8 @@ class UnFollowUser(HTTPMethodView):
     decorators = []
 
     async def get(self, request: Request) -> Union[json, NoReturn]:
-        pass
+        user_id = TokenHelper.extract_from_request(
+            request=request).decode().user_id
+        validator = UnFollowUserRequestSchema().load(data=request.form)
+        if validator.errors:
+            raise ValidationErrorException
