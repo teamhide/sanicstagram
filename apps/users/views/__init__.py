83e0f6536892a145bfd1f191eba2c346b6eb67df
John <john@cupist.com>
2019-08-28 00:00:41 +0900

    Register FollowUser/UnFollowUser/ExploreUsers/UserFollowers/UserFollowings in urls.py

--- apps/users/views/__init__.py
+++ apps/users/views/__init__.py
@@ -1,7 +1,13 @@
 from sanic import Blueprint
 
-from apps.users.views.v1 import User, UserList
+from apps.users.views.v1 import (User, UserList, FollowUser, UnFollowUser,
+                                 ExploreUsers, UserFollowers, UserFollowings)
 
 bp = Blueprint('users', url_prefix='/api/v1')
 bp.add_route(User.as_view(), '/users/<id:int>')
 bp.add_route(UserList.as_view(), '/users')
+bp.add_route(FollowUser.as_view(), '/<id:int>/follow')
+bp.add_route(UnFollowUser.as_view(), '/<id:int>/unfollow')
+bp.add_route(ExploreUsers.as_view(), '/explore')
+bp.add_route(UserFollowers.as_view(), '/<id:int>/followers')
+bp.add_route(UserFollowings.as_view(), '/<id:int>/followings')
