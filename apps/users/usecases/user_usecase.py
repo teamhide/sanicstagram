c16ad31522b37937289c4a40c8e39f1154378152
john <john@cupist.com>
2019-08-28 13:33:29 +0900

    Implement Follow/Unfollow user
    1. Implement FollowUserUsecase/UnFollowUserUsecase
    2. Remove repositories foleder at users

--- apps/users/usecases/user_usecase.py
+++ apps/users/usecases/user_usecase.py
@@ -1,10 +1,20 @@
 from apps.users.dtos import (FollowUserDto, UnFollowUserDto)
-from apps.users.repositories.mysql import UserMySQLRepository
+from core.databases import session
+from apps.users.models import User, Follow
+import sqlalchemy.exc
 
 
 class UserUsecase:
     def __init__(self):
-        self.repository = UserMySQLRepository()
+        pass
+
+    def _is_followed(self, user_id: int, follow_user_id: int) -> bool:
+        is_exist = session.query(Follow)\
+            .filter(
+            Follow.follower_id == user_id,
+            Follow.following_id == follow_user_id,
+        ).first()
+        return is_exist is not None
 
 
 class RegisterUserUsecase(UserUsecase):
@@ -23,13 +33,43 @@ class UpdateUserUsecase(UserUsecase):
 
 
 class FollowUserUsecase(UserUsecase):
-    def execute(self, dto: FollowUserDto):
-        pass
+    def execute(self, dto: FollowUserDto) -> None:
+        if self._is_followed(
+                user_id=dto.user_id,
+                follow_user_id=dto.follow_user_id,
+        ):
+            return
+
+        try:
+            relationship = Follow(
+                follower_id=dto.user_id,
+                following_id=dto.follow_user_id,
+            )
+            session.add(relationship)
+            session.commit()
+        except sqlalchemy.exc.IntegrityError as e:
+            print(e)
+            session.rollback()
 
 
 class UnFollowUserUsecase(UserUsecase):
-    def execute(self, dto: UnFollowUserDto):
-        pass
+    def execute(self, dto: UnFollowUserDto) -> None:
+        if not self._is_followed(
+                user_id=dto.user_id,
+                follow_user_id=dto.follow_user_id,
+        ):
+            return
+
+        try:
+            relationship = Follow(
+                follower_id=dto.user_id,
+                following_id=dto.follow_user_id,
+            )
+            session.delete(relationship)
+            session.commit()
+        except sqlalchemy.exc.IntegrityError as e:
+            print(e)
+            session.rollback()
 
 
 class LoginUsecase(UserUsecase):
